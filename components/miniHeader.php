4a4a635469236e2f00139af167b78e8883ba7f8b
Sanjar Ibrokhimov <sanjaribrokhimov3210@mail.ru>
2025-02-10 12:43:17 +0500

    fix card center css

--- components/miniHeader.php
+++ components/miniHeader.php
@@ -9,7 +9,7 @@ $IP = '144.126.128.67';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bloger Agency</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css?v=1.0.1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
 
