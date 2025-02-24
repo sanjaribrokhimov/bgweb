45d98a2da8d15f1f5bed2de8ba15c0e9f7b0678d
dior <l1nton707shw@gmail.com>
2025-02-24 16:57:02 +0500

    close TgWebApp on click logout button

--- logout.php
+++ logout.php
@@ -11,6 +11,16 @@ session_destroy();
     localStorage.clear();
     // Перенаправляем на страницу входа
     window.location.href = 'login.php';
+
+    function closeTelegramWebApp() {
+        if (window.TelegramWebviewProxy) {
+        window.TelegramWebviewProxy.postEvent('web_app_close');
+        } else if(window.Telegram && window.Telegram.WebApp) {
+            window.Telegram.WebApp.close();
+        }
+    }
+    closeTelegramWebApp();
+
     </script>
 </head>
 </html> 
