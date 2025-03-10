4718201687e9cd2edb450b7154ee9ba25fd2c952
Sanjar Ibrokhimov <sanjaribrokhimov3210@mail.ru>
2025-03-10 15:12:42 +0500

    asd

--- api/utils/telegram.go
+++ api/utils/telegram.go
@@ -5,16 +5,34 @@ import (
 	"net/http"
 )
 
-// SendTelegramOTP отправляет OTP код через Telegram бот
+// SendTelegramOTP отправляет OTP код через Telegram бот с высоким приоритетом
 func SendTelegramOTP(chatID string, otp string) error {
 	botToken := "7690904808:AAEyzgbEZ3--sQ1pkJ-bFBpnDSCY2rNq9VY"
 
-	// Используем HTML-форматирование для выделения кода
+	// Сначала отправляем пустое сообщение с уведомлением
+	notificationURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+	client := &http.Client{}
+	notifReq, err := http.NewRequest("POST", notificationURL, nil)
+	if err != nil {
+		return fmt.Errorf("error creating notification request: %v", err)
+	}
+
+	q := notifReq.URL.Query()
+	q.Add("chat_id", chatID)
+	q.Add("text", "⚠️ Новый код подтверждения")
+	notifReq.URL.RawQuery = q.Encode()
+
+	_, err = client.Do(notifReq)
+	if err != nil {
+		return fmt.Errorf("error sending notification: %v", err)
+	}
+
+	// Затем отправляем сам код
 	message := fmt.Sprintf("<b>🔔 Ваш код подтверждения:</b> <code>%s</code>", otp)
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
 
 	// Создаем HTTP клиент
-	client := &http.Client{}
+	client = &http.Client{}
 
 	// Формируем запрос
 	req, err := http.NewRequest("POST", url, nil)
@@ -23,12 +41,14 @@ func SendTelegramOTP(chatID string, otp string) error {
 	}
 
 	// Добавляем параметры
-	q := req.URL.Query()
+	q = req.URL.Query()
 	q.Add("chat_id", chatID)
 	q.Add("text", message)
-	q.Add("parse_mode", "HTML") // Указываем режим парсинга HTML
+	q.Add("parse_mode", "HTML")
 	q.Add("disable_notification", "false")
 	q.Add("allow_sending_without_reply", "true")
+	q.Add("disable_web_page_preview", "true") // Отключаем предпросмотр ссылок
+	q.Add("protect_content", "false")         // Убираем защиту контента
 	req.URL.RawQuery = q.Encode()
 
 	// Отправляем запрос
